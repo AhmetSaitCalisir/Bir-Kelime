1a092e84d04972c6987e78e7b7d9507ed8868680
nilguresci <54067949+nilguresci@users.noreply.github.com>
2020-05-21 12:11:07 +0300

    Update Form_Kelime.Designer.cs

--- Bir Kelime/Bir Kelime/Form_Kelime.Designer.cs
+++ Bir Kelime/Bir Kelime/Form_Kelime.Designer.cs
@@ -43,20 +43,22 @@
             this.label_Harfler.AutoSize = true;
             this.label_Harfler.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label_Harfler.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(7)))), ((int)(((byte)(64)))));
-            this.label_Harfler.Location = new System.Drawing.Point(337, 48);
+            this.label_Harfler.Location = new System.Drawing.Point(449, 59);
+            this.label_Harfler.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_Harfler.Name = "label_Harfler";
-            this.label_Harfler.Size = new System.Drawing.Size(224, 40);
+            this.label_Harfler.Size = new System.Drawing.Size(267, 51);
             this.label_Harfler.TabIndex = 0;
-            this.label_Harfler.Text = "1 2 3 4 5 6 7 8";
+            this.label_Harfler.Text = "a b c d e f g ğ";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(7)))), ((int)(((byte)(64)))));
-            this.label1.Location = new System.Drawing.Point(25, 48);
+            this.label1.Location = new System.Drawing.Point(33, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(226, 40);
+            this.label1.Size = new System.Drawing.Size(284, 51);
             this.label1.TabIndex = 1;
             this.label1.Text = "Gelen Harfler";
             // 
@@ -65,9 +67,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(78)))), ((int)(((byte)(80)))));
-            this.label2.Location = new System.Drawing.Point(25, 108);
+            this.label2.Location = new System.Drawing.Point(33, 133);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(257, 40);
+            this.label2.Size = new System.Drawing.Size(324, 51);
             this.label2.TabIndex = 2;
             this.label2.Text = "Bulunan Kelime";
             // 
@@ -76,20 +79,22 @@
             this.label_Kelime.AutoSize = true;
             this.label_Kelime.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label_Kelime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(78)))), ((int)(((byte)(80)))));
-            this.label_Kelime.Location = new System.Drawing.Point(337, 108);
+            this.label_Kelime.Location = new System.Drawing.Point(449, 133);
+            this.label_Kelime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_Kelime.Name = "label_Kelime";
-            this.label_Kelime.Size = new System.Drawing.Size(238, 40);
+            this.label_Kelime.Size = new System.Drawing.Size(203, 51);
             this.label_Kelime.TabIndex = 3;
-            this.label_Kelime.Text = "EQWERTYUI";
+            this.label_Kelime.Text = "KELİME";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold);
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(7)))), ((int)(((byte)(64)))));
-            this.label4.Location = new System.Drawing.Point(25, 170);
+            this.label4.Location = new System.Drawing.Point(33, 209);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(95, 40);
+            this.label4.Size = new System.Drawing.Size(116, 51);
             this.label4.TabIndex = 4;
             this.label4.Text = "Puan";
             // 
@@ -98,9 +103,10 @@
             this.label_Puan.AutoSize = true;
             this.label_Puan.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold);
             this.label_Puan.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(7)))), ((int)(((byte)(64)))));
-            this.label_Puan.Location = new System.Drawing.Point(338, 170);
+            this.label_Puan.Location = new System.Drawing.Point(451, 209);
+            this.label_Puan.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_Puan.Name = "label_Puan";
-            this.label_Puan.Size = new System.Drawing.Size(53, 40);
+            this.label_Puan.Size = new System.Drawing.Size(66, 51);
             this.label_Puan.TabIndex = 5;
             this.label_Puan.Text = "10";
             // 
@@ -109,9 +115,10 @@
             this.button_Tekrarla.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_Tekrarla.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button_Tekrarla.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(7)))), ((int)(((byte)(63)))));
-            this.button_Tekrarla.Location = new System.Drawing.Point(175, 240);
+            this.button_Tekrarla.Location = new System.Drawing.Point(223, 295);
+            this.button_Tekrarla.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_Tekrarla.Name = "button_Tekrarla";
-            this.button_Tekrarla.Size = new System.Drawing.Size(239, 40);
+            this.button_Tekrarla.Size = new System.Drawing.Size(319, 49);
             this.button_Tekrarla.TabIndex = 6;
             this.button_Tekrarla.Text = "Tekrarla Oynat";
             this.button_Tekrarla.UseVisualStyleBackColor = true;
@@ -122,9 +129,10 @@
             this.button_Cikis.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_Cikis.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button_Cikis.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(7)))), ((int)(((byte)(63)))));
-            this.button_Cikis.Location = new System.Drawing.Point(567, 3);
+            this.button_Cikis.Location = new System.Drawing.Point(756, 4);
+            this.button_Cikis.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_Cikis.Name = "button_Cikis";
-            this.button_Cikis.Size = new System.Drawing.Size(25, 25);
+            this.button_Cikis.Size = new System.Drawing.Size(33, 31);
             this.button_Cikis.TabIndex = 7;
             this.button_Cikis.Text = "X";
             this.button_Cikis.UseVisualStyleBackColor = true;
@@ -132,10 +140,10 @@
             // 
             // Form_Kelime
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(29)))));
-            this.ClientSize = new System.Drawing.Size(595, 327);
+            this.ClientSize = new System.Drawing.Size(793, 402);
             this.Controls.Add(this.button_Cikis);
             this.Controls.Add(this.button_Tekrarla);
             this.Controls.Add(this.label_Puan);
@@ -146,6 +154,7 @@
             this.Controls.Add(this.label_Harfler);
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(7)))), ((int)(((byte)(64)))));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form_Kelime";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Kelime";
